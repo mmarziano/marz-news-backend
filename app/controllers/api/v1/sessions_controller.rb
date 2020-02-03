44d05b9230ceb3da3c5aba836df01793a0718ef9
Monica Marziano <mmarziano1@gmail.com>
2020-02-03 11:00:21 -0500

    add sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::SessionsController < ApplicationController
+    
+    def index
+        @user = User.new
+    end
+
+    def new
+        @user=User.new
+    end 
+
+    def create
+        @user = User.find_by(email: params[:email])
+            if @user && @user.authenticate(params[:password])&& @user.school_id != nil
+                log_in(@user)
+                redirect_to user_path(@user)
+            elsif @user && @user.authenticate(params[:password])
+                redirect_to signup_path(@user)
+            elsif @user 
+                render "sessions/login_retry"
+            else 
+                redirect_to new_user_path
+            end
+    end 
+
+
+    def destroy 
+        session[:user_id] = nil
+
+        redirect_to '/'
+    end 
+
+    def omniauth
+        @user = User.from_omniauth(auth)
+        @user.save
+        session[:user_id] = @user.id
+        if @user.household_id == nil
+            render '/users/complete_profile', layout: "main"
+        else 
+            redirect_to user_path(@user)
+        end 
+    end
+
+    private
+    
+    def auth
+        request.env['omniauth.auth']
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, only: [:create]
+      post '/login', to: 'sessions#create'
+      get '/preferences', to: 'users#preferences'
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
